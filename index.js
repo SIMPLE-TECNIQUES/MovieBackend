1e6d796577a08f6006bbae366d45b2a2a4fadb8c
SIMPLE-TECNIQUES <shankarcode2000@gmail.com>
2024-10-31 23:08:10 +0530

    Cors handling

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const server = http.createServer(app);
 connectDB();
 const io = new Server(server, {
     cors: {
-        origin: ['*','http://localhost:5173', 'http://localhost:5174'],
+        origin: ['http://localhost:5173', 'http://localhost:5174',"https://criticsmohanadmin.netlify.app","https://criticsmohanmoviwreview.netlify.app"],
         methods: ["GET", "POST"],
         credentials: true,
     },
 });
 const corsOptions = {
-    origin: ['*','http://localhost:5173', 'http://localhost:5174'], 
+    origin: ['http://localhost:5173', 'http://localhost:5174',"https://criticsmohanadmin.netlify.app","https://criticsmohanmoviwreview.netlify.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"], 
     credentials: true, 
 };
